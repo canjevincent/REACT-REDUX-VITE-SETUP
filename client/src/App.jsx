b74135fc8ef33da3ba1d72a00b76b13588fa991c
VincentValentine <valentinen39@yahoo.com>
2023-11-27 19:58:21 +0800

    First commit.

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import Counter from "./features/counter/Counter"
+import PostsList from "./features/posts/PostsList"
+import AddPostForm from "./features/posts/AddPostForm"
+
+function App() {
+  return (
+    <main className="App">
+      {/* <Counter/> */}
+      <AddPostForm />
+      <PostsList />
+    </main>
+  )
+}
+
+export default App
---
+++ client/src/features/posts/postsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from "@reduxjs/toolkit"
+
+const initialState = [
+  {id:'1', title: 'Learning Redux Toolkit', content: "I've heard good things."},
+  {id:'2', title: 'Slices...', content: "The more I say slice, the more I want pizza."}
+]
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content) { // Use prepare to modify objects before sending to reducer
+        return{ 
+          payload: {
+            id: nanoid(),
+            title,
+            content
+          }
+        }
+      }
+    },
+  }
+})
+
+export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded } = postsSlice.actions
+
+export default postsSlice.reducer
