5a004116849f5be6383e91b686fd1fba361b85a9
Vincent Canje <canjevincent@gmail.com>
2023-12-03 13:38:38 +0800

    New Commit

--- client/src/features/posts/AddPostForm.jsx
+++ client/src/features/posts/AddPostForm.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 
 import { postAdded } from "./postsSlice"
+import { selectAllUsers } from "../users/usersSlice"
 
 const AddPostForm = () => {
 
@@ -9,21 +10,32 @@ const AddPostForm = () => {
 
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("") 
+  const [userId, setUserId] = useState("")
+
+  const users = useSelector(selectAllUsers)
 
   const onTitleChanged = e => setTitle(e.target.value)
   const onContentChanged = e => setContent(e.target.value)
+  const onAuthorChanged = e => setUserId(e.target.value)
 
   const onSavePostClicked = () => {
     if (title && content) {
       dispatch(
-        postAdded(title,content)
+        postAdded(title, content, userId)
       )
-
       setTitle("")
       setContent("")
 
     }
   }
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+  const userOptions = users.map(user => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ))
 
   return (
       <section>
@@ -37,6 +49,11 @@ const AddPostForm = () => {
             value={title}
             onChange={onTitleChanged}
           />
+          <label htmlFor="postUathor">Author:</label>
+          <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+            <option value=""></option>
+            {userOptions}
+          </select>
           <label htmlFor="postContent">Content:</label>
           <textarea
             id="postContent"
@@ -47,6 +64,7 @@ const AddPostForm = () => {
           <button 
             type="button"
             onClick={onSavePostClicked}
+            disabled={!canSave}
           >Save Post</button>
         </form>
       </section>
--- client/src/features/posts/postsSlice.js
+++ client/src/features/posts/postsSlice.js
@@ -1,9 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 import { nanoid } from "@reduxjs/toolkit"
+import { sub } from "date-fns"
 
 const initialState = [
-  {id:'1', title: 'Learning Redux Toolkit', content: "I've heard good things."},
-  {id:'2', title: 'Slices...', content: "The more I say slice, the more I want pizza."}
+  {id:'1', 
+   title: 'Learning Redux Toolkit', 
+   content: "I've heard good things.",
+   date: sub(new Date(), { minutes: 10}).toISOString(),
+   reactions: {
+    thumbsUp: 0,
+    wow: 0,
+    heart: 0,
+    rocket: 0,
+    coffee: 0,
+   }
+  },
+  {id:'2', 
+   title: 'Slices...', 
+   content: "The more I say slice, the more I want pizza.",
+   date: sub(new Date(), { minutes: 5}).toISOString(),
+   reactions: {
+    thumbsUp: 0,
+    wow: 0,
+    heart: 0,
+    rocket: 0,
+    coffee: 0,
+   }
+  }
 ]
 
 const postsSlice = createSlice({
@@ -14,21 +37,37 @@ const postsSlice = createSlice({
       reducer(state, action) {
         state.push(action.payload)
       },
-      prepare(title, content) { // Use prepare to modify objects before sending to reducer
+      prepare(title, content, userId) { // Use prepare to modify objects before sending to reducer
         return{ 
           payload: {
             id: nanoid(),
             title,
-            content
+            content,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0
+            }
           }
         }
       }
     },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    }
   }
 })
 
 export const selectAllPosts = (state) => state.posts;
 
-export const { postAdded } = postsSlice.actions
+export const { postAdded, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
